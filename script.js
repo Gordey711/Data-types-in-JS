435797bd2227d9e991725edbce99235d957a6744
Gordey <gordeylog71@gmail.com>
2022-12-24 23:07:32 +0300

    Data types in JS

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Тип данных цифра
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+// Тип данных Строка
+
+const person = 'Alex';
+const age = '5';
+
+// Логический тип данных/boolean
+
+const bool = false;
+const boolean = true;
+
+// null and undefined
+
+console.log(something);
+
+let und;
+console.log(und);
+
+// Обьекты 
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+// console.log(obj['name']);
+
+// Массив
+
+let arr = ['plum.png', 'oranje.jpg', 'aplle.svg', 6, {}, []];
+console.log(arr[0]);
